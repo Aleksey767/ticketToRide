60fdd067960216007e8146df278df115efed617f
Aleksey767 <al_sazonov@list.ru>
2024-07-19 09:28:41 +0200

    change entity files

--- src/main/java/com/andersen/ticketToRide/model/Ticket.java
+++ src/main/java/com/andersen/ticketToRide/model/Ticket.java
@@ -1,5 +1,44 @@
 package com.andersen.ticketToRide.model;
 
+import com.andersen.ticketToRide.enums.Cities;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+
+import java.math.BigDecimal;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "ticket")
 public class Ticket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    private Cities departure;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    private Cities arrival;
+
+    private int segments;
+
+    private BigDecimal price;
+
+    private String currency;
+
+    private int traveller_amount;
+
+    @ManyToOne
+    @JoinColumn(name = "traveller_id", nullable = false)
+    private Traveller traveller;
 }
--- src/main/java/com/andersen/ticketToRide/model/Traveller.java
+++ src/main/java/com/andersen/ticketToRide/model/Traveller.java
@@ -1,5 +1,31 @@
 package com.andersen.ticketToRide.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "traveller")
 public class Traveller {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+    private String email;
+
+    private BigDecimal balance;
 }
