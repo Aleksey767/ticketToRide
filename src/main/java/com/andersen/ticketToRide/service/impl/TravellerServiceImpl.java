ced5af356f772e3e64c79f2a38e4a748069cf539
Aleksey767 <al_sazonov@list.ru>
2024-07-19 09:29:54 +0200

    fix ServiceImpl files

--- src/main/java/com/andersen/ticketToRide/service/impl/TravellerServiceImpl.java
+++ src/main/java/com/andersen/ticketToRide/service/impl/TravellerServiceImpl.java
@@ -1,5 +1,27 @@
 package com.andersen.ticketToRide.service.impl;
 
-public class TravellerServiceImpl {
+import com.andersen.ticketToRide.dto.TravellerDto;
+import com.andersen.ticketToRide.mapper.TravellerMapper;
+import com.andersen.ticketToRide.model.Traveller;
+import com.andersen.ticketToRide.repository.TravellerRepository;
+import com.andersen.ticketToRide.service.TravellerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
+public class TravellerServiceImpl implements TravellerService {
+    @Autowired
+    private TravellerRepository travellerRepository;
+
+    @Override
+    public void saveTraveller(TravellerDto travellerDto) {
+        Traveller traveller = TravellerMapper.mapToTraveller(travellerDto);
+        travellerRepository.save(traveller);
+    }
+    @Override
+    public TravellerDto getTravellerById(long id){
+        return TravellerMapper.mapToTravellerDto(travellerRepository.findById(id).get());
+    }
 }
