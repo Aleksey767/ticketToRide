19681355e49b044c373967203e44e7891799a5c4
Aleksey767 <al_sazonov@list.ru>
2024-07-19 09:33:46 +0200

    small fixes

--- src/main/java/com/andersen/ticketToRide/repository/TicketRepository.java
+++ src/main/java/com/andersen/ticketToRide/repository/TicketRepository.java
@@ -1,5 +1,10 @@
 package com.andersen.ticketToRide.repository;
 
-public interface TicketRepository {
+import com.andersen.ticketToRide.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 }
