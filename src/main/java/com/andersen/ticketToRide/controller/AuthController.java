975a21e0c01b1ae685bd3ceebcddc0104e16f878
Aleksey767 <al_sazonov@list.ru>
2024-07-21 10:04:12 +0200

    change controllers

--- src/main/java/com/andersen/ticketToRide/controller/AuthController.java
+++ src/main/java/com/andersen/ticketToRide/controller/AuthController.java
@@ -1,8 +1,34 @@
 package com.andersen.ticketToRide.controller;
 
+import com.andersen.ticketToRide.dto.UserDto;
+import com.andersen.ticketToRide.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@AllArgsConstructor
 @Controller
 public class AuthController {
 
+    UserService userService;
+
+    @GetMapping("/login")
+    public String myLogin() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registrationSubmit(@ModelAttribute("user") UserDto userDto,Model model) {
+        userService.saveUser(userDto);
+        return "redirect:/login";
+    }
 }
