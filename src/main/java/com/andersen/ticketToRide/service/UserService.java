4c7529dbae0227e70aab3c9e7625d5f728bee2c7
Aleksey767 <al_sazonov@list.ru>
2024-07-21 15:25:24 +0200

    add function updateUserBalance()

--- src/main/java/com/andersen/ticketToRide/service/UserService.java
+++ src/main/java/com/andersen/ticketToRide/service/UserService.java
@@ -2,9 +2,10 @@ package com.andersen.ticketToRide.service;
 
 import com.andersen.ticketToRide.dto.UserDto;
 
-public interface UserService {
+import java.math.BigDecimal;
 
+public interface UserService {
     void saveUser(UserDto userDto);
-    UserDto getUserById(Long id);
     UserDto getUserByUsername(String username);
+    void updateUserBalance(String username, BigDecimal balance);
 }
