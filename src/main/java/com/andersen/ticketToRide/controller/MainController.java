9a297ab50774043fea22d37b222ab056d1909c56
Aleksey767 <al_sazonov@list.ru>
2024-07-21 15:25:57 +0200

    change MainController

--- src/main/java/com/andersen/ticketToRide/controller/MainController.java
+++ src/main/java/com/andersen/ticketToRide/controller/MainController.java
@@ -6,11 +6,13 @@ import com.andersen.ticketToRide.enums.Cities;
 import com.andersen.ticketToRide.mapper.UserMapper;
 import com.andersen.ticketToRide.service.TicketService;
 import com.andersen.ticketToRide.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.security.Principal;
 
 @Controller
@@ -44,24 +46,33 @@ public class MainController {
         return "main";
     }
 
+    @Transactional
     @GetMapping("/{username}")
     public String userProfile(@PathVariable String username, Model model, Principal principal) {
         UserDto userDto = userService.getUserByUsername(principal.getName());
         model.addAttribute("tickets", ticketService.getAllTicketsByUser(userDto));
+        model.addAttribute("balance", userDto.getBalance());
         return "userPage";
     }
 
-    @GetMapping("/new_ticket")
-    public String newTicket(Model model) {
-        model.addAttribute("ticketDto", new TicketDto());
-        return "newTicket";
-    }
-
+    @Transactional
     @PostMapping("/add_ticket")
-    public String addTicket(@ModelAttribute TicketDto ticketDto, Principal principal) {
-        UserDto userDto = userService.getUserByUsername(principal.getName());
+    public String addTicket(@ModelAttribute TicketDto ticketDto, Principal principal, Model model) {
+        String username = principal.getName();
+        if (ticketDto.getDeparture() == null || ticketDto.getArrival() == null || ticketDto.getArrival().equals(ticketDto.getDeparture())) {
+            model.addAttribute("errorMessage", "Please choose arrival and departure");
+            model.addAttribute("cities", Cities.values());
+            return "main";
+        }
+        UserDto userDto = userService.getUserByUsername(username);
         ticketDto.setUser(UserMapper.mapToUser(userDto));
         ticketService.saveTicket(ticketDto);
-        return "redirect:/main";
+
+        BigDecimal result = userDto.getBalance().subtract(BigDecimal.TEN);// stub!
+        userService.updateUserBalance(username, result);
+
+        model.addAttribute("successMessage", "Ticket was successfully purchased!");
+        model.addAttribute("cities", Cities.values());
+        return "main";
     }
 }
