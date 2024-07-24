e0be87a1c46afb8045803cb3ca5b1f435b460d10
Aleksey767 <al_sazonov@list.ru>
2024-07-24 13:11:48 +0200

    add Java doc to interfaces

--- src/main/java/com/andersen/ticketToRide/service/TicketService.java
+++ src/main/java/com/andersen/ticketToRide/service/TicketService.java
@@ -5,7 +5,24 @@ import com.andersen.ticketToRide.dto.UserDto;
 
 import java.util.List;
 
+/**
+ * The {@code TicketService} interface provides methods to manage ticket information in the ticket-to-ride application.
+ * It allows for saving tickets and retrieving all tickets associated with a specific user.
+ */
 public interface TicketService {
+
+    /**
+     * Saves a new ticket to the postgre.
+     *
+     * @param ticketDto a {@link TicketDto} object containing the ticket details to be saved
+     */
     void saveTicket(TicketDto ticketDto);
+
+    /**
+     * Retrieves all tickets associated with a specific user.
+     *
+     * @param userDto a {@link UserDto} object representing the user whose tickets are to be retrieved
+     * @return a {@link List} of {@link TicketDto} objects representing the user's tickets
+     */
     List<TicketDto> getAllTicketsByUser(UserDto userDto);
 }
