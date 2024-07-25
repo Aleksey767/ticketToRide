ed85a754a8ec27e17bccc9f146f98406f45ccfc0
Aleksey767 <al_sazonov@list.ru>
2024-07-25 17:27:34 +0200

    fix /api/ticket/calculate_price

--- src/main/java/com/andersen/ticketToRide/controller/AuthController.java
+++ src/main/java/com/andersen/ticketToRide/controller/AuthController.java
@@ -5,14 +5,9 @@ import com.andersen.ticketToRide.service.UserService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @AllArgsConstructor
 @Controller
@@ -24,7 +19,7 @@ public class AuthController {
 
     @GetMapping("/login")
     public String myLogin() {
-        LOGGER.info("[INFO MESSAGE]: GET request to /login");
+        LOGGER.info("GET request to /login");
         return "login";
     }
 
@@ -33,21 +28,5 @@ public class AuthController {
         LOGGER.info("GET request to /registration");
         model.addAttribute("user", new UserDto());
         return "registration";
-    }
-
-    @GetMapping("/success_after_creating_user")
-    public String redirect(RedirectAttributes redirectAttributes) {
-        LOGGER.info("GET request to /success_after_creating_user");
-
-        redirectAttributes.addFlashAttribute("successMessage", "User was successfully created");
-        return "redirect:/login";
-    }
-
-    @PostMapping("/api/user/save_user")
-    public ResponseEntity<?> registrationSubmit(@ModelAttribute UserDto userDto) {
-        LOGGER.info("[INFO MESSAGE]: POST request to /api/user/save_user");
-        userService.saveUser(userDto);
-
-        return ResponseEntity.ok().body(userDto);
     }
 }
--- src/main/resources/static/js/add-ticket-handler.js
+++ src/main/resources/static/js/add-ticket-handler.js
@@ -1,44 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            const value = this.getAttribute('data-city');
+            const inputId = this.getAttribute('data-input');
+            const dropdownButton = document.getElementById(this.getAttribute('data-target'));
+
+            document.getElementById(inputId).value = value;
+            dropdownButton.textContent = value;
+        });
+    });
+});
+
+const calculatePriceForm = document.getElementById('calculate-price-form');
+if (calculatePriceForm) {
+    calculatePriceForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(calculatePriceForm);
+        fetch(calculatePriceForm.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(data => {
+                document.querySelector('#dropdownMenuButton-1').textContent = data.departure;
+                document.querySelector('#dropdownMenuButton-2').textContent = data.arrival;
+                document.querySelector('#dropdownMenuButton-3').textContent = data.travellerAmount;
+
+                const priceElement = document.querySelector('h1.mt-3.display-5');
+                if (priceElement) {
+                    priceElement.textContent = `${data.price} GBP`;
+                }
+
+                document.getElementById('departure').value = data.departure;
+                document.getElementById('arrival').value = data.arrival;
+                document.getElementById('travellerAmount').value = data.travellerAmount;
+                document.getElementById('price').value = data.price;
+
+                document.getElementById('selected-departure').value = data.departure;
+                document.getElementById('selected-arrival').value = data.arrival;
+                document.getElementById('selected-travellerAmount').value = data.travellerAmount;
+                document.getElementById('saved-price').value = data.price;
+            }).catch(error => {
+            console.error('Error:', error);
+        });
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('save_ticket-form');
     if (form) {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
-
             const formData = new FormData(form);
-
             fetch(form.action, {
                 method: 'POST',
                 body: formData,
                 headers: {
                     'Accept': 'application/json'
                 }
-            })
-                .then(response => {
-                    if (response.ok) {
-                        const redirectHeader = response.headers.get('X-Redirect');
-                        let redirectUrl = '/main';
-                        switch (redirectHeader) {
-                            case 'invalid_data':
-                                redirectUrl = '/redirect_invalid_data';
-                                break;
-                            case 'lack_of_money':
-                                redirectUrl = '/redirect_lack_of_money';
-                                break;
-                            case 'success':
-                                redirectUrl = '/redirect_success';
-                                break;
-                            default:
-                                console.warn('Unknown redirect header:', redirectHeader);
-                                break;
-                        }
-                        window.location.href = redirectUrl;
-                    } else {
-                        throw new Error('Network response is not ok.');
+            }).then(response => {
+                if (response.ok) {
+                    const redirectHeader = response.headers.get('X-Redirect');
+                    let redirectUrl = '/main';
+                    switch (redirectHeader) {
+                        case 'invalid_data':
+                            redirectUrl = '/redirect_to_main?status=invalid_data';
+                            break;
+                        case 'out_of_money':
+                            redirectUrl = '/redirect_to_main?status=out_of_money';
+                            break;
+                        case 'success':
+                            redirectUrl = '/redirect_to_main?status=success';
+                            break;
+                        default:
+                            console.warn('Unknown redirect header:', redirectHeader);
+                            break;
                     }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
+                    window.location.href = redirectUrl;
+                } else {
+                    throw new Error('Network response is not ok.');
+                }
+            }).catch(error => {
+                console.error('Error:', error);
+            });
         });
     }
 });
